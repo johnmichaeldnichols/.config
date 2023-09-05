9d5d0fb44f0ef18237a9c5e26f161e4b54953680
JohnMichael Nichols <46973377+johnmichaeldnichols@users.noreply.github.com>
2023-09-05 13:59:05 -0400

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+print('hey jm, I am your new lua based coding tool!')
+
+require("keymaps")
+require("plugins")
---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,2 @@
+message = 'hello jm'
+vim.keymap.set('n', '<leader>h', print(message))
